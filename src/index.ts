c5fe58f0db6ce57b020b5d5d0242e4781008585c
codye1 <123339584+codye1@users.noreply.github.com>
2026-01-06 00:50:53 +0200

    add prisma

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
+import { prisma } from './lib/prisma';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,26 @@ app.get('/', (req: Request, res: Response) => {
   res.send({ message: 'Hello World!' });
 });
 
+// GET: Fetch all users
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+// POST: Create a new user
+app.post('/users', async (req: Request, res: Response) => {
+  const { email, name } = req.body;
+  try {
+    const newUser = await prisma.user.create({
+      data: { email, name },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: 'User already exists or invalid data' });
+  }
+});
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Example app listening on port http://localhost:${PORT}`);
 });
