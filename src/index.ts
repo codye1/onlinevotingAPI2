9d393cf4049a7a2ddbebef85106bd438a245754c
codye1 <123339584+codye1@users.noreply.github.com>
2026-01-20 17:22:35 +0200

    prepared to deploy

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import router from './router/router';
 import cors, { CorsOptions } from 'cors';
 
+import router from './router/router';
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,41 +19,22 @@ const corsOptions: CorsOptions = {
     if (!origin) {
       return callback(null, true);
     }
+
     if (corsAllowList.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+      return callback(null, true);
     }
+
+    callback(new Error('Not allowed by CORS'));
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 };
-app.use(cors(corsOptions));
 
+app.use(cors(corsOptions));
 app.use(router);
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port http://localhost:${PORT}`);
+  console.log(`API listening on http://localhost:${PORT}`);
 });
-
-/*
-// GET: Fetch all users
-app.get('/users', async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
-
-// POST: Create a new user
-app.post('/users', async (req: Request, res: Response) => {
-  const { email, name } = req.body;
-  try {
-    const newUser = await prisma.user.create({
-      data: { email, name },
-    });
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error(error);
-    res.status(400).json({ error: 'User already exists or invalid data' });
-  }
-});
-*/
